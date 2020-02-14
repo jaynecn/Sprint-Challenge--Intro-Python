57353a75d9a61b026a0a85e6e4c1791eea6ac400
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-02-14 14:10:44 +0000

    erros in oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -9,7 +9,42 @@
 # [Car]  [Motorcycle]
 
 class Vehicle:
-  def __init__
+  def __init__(self, name):
+    self.name = name
+    self.flight_vehicle = FlightVehicle
+    self.starship = Starship
+  pass
+
+class GroundVehicle(Vehicle):
+  def __init__(self, name):
+    super().__init__()
+    self.name = name
+  pass
+
+class Car(GroundVehicle):
+  def __init__(self, name):
+    self.name = name
+  pass
+
+class Motorcycle(GroundVehicle):
+  def __init__(self, name):
+    self.name = name
+  pass
+
+class FlightVehicle:
+  def __init__(self, name):
+    self.name = name
+  pass
+
+class Airplane(FlightVehicle):
+  def __init__(self, name):
+    self.name = name
+  pass
+
+class Starship:
+  def __init__(self, name):
+    self.name = name
+  pass
 
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
