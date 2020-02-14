e31b27d8a4732c46117dfc8d6a2d4def00b4bfd7
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-02-14 14:15:20 +0000

    worked out oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -8,43 +8,6 @@
 #   v       v
 # [Car]  [Motorcycle]
 
-class Vehicle:
-  def __init__(self, name):
-    self.name = name
-    self.flight_vehicle = FlightVehicle
-    self.starship = Starship
-  pass
-
-class GroundVehicle(Vehicle):
-  def __init__(self, name):
-    super().__init__()
-    self.name = name
-  pass
-
-class Car(GroundVehicle):
-  def __init__(self, name):
-    self.name = name
-  pass
-
-class Motorcycle(GroundVehicle):
-  def __init__(self, name):
-    self.name = name
-  pass
-
-class FlightVehicle:
-  def __init__(self, name):
-    self.name = name
-  pass
-
-class Airplane(FlightVehicle):
-  def __init__(self, name):
-    self.name = name
-  pass
-
-class Starship:
-  def __init__(self, name):
-    self.name = name
-  pass
 
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
@@ -56,3 +19,25 @@ class Starship:
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle():
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass 
