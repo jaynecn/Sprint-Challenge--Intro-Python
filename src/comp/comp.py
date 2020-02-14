bba350edc6f1bbf1e55e1dcabe43a49c952dc805
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-02-14 08:34:41 +0000

    ends with e

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -23,14 +23,22 @@ humans = [
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
-print("Starts with D:")
+print("Start with D")
 a = []
+for i in humans:
+    new_list = (i.name[0])
+    if new_list == 'D':
+        a.append(i.name)
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
 b = []
+for i in humans:
+    e_list = (i.name[-1])
+    if e_list == 'e':
+        b.append(i.name)
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
